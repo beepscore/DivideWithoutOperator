b6d2fcb3dc5f5e98c37b08415bdaa55f332a48f6
beepscore <support@beepscore.com>
2010-12-11 12:47:01 -0800

    Initial commit.  In .gitignore, changed .DS_Store? to .DS_Store and Icon? to Icon

---
+++ src/com/beepscore/DivideWithoutOperator/DivideWithoutOperator.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.beepscore.DivideWithoutOperator;
+
+/**
+ * @author stevebaker
+ *
+ */
+public class DivideWithoutOperator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
