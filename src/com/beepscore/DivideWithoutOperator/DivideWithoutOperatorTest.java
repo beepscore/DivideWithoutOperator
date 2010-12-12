93dbff0d45606a6b0c99161e810d473d6526274d
beepscore <support@beepscore.com>
2010-12-11 17:18:11 -0800

    Add tests.  All pass, except for one that is commented out. See TODOs.  Could list TODOs as github issues.

--- src/com/beepscore/DivideWithoutOperator/DivideWithoutOperatorTest.java
+++ src/com/beepscore/DivideWithoutOperator/DivideWithoutOperatorTest.java
@@ -30,7 +30,32 @@ public class DivideWithoutOperatorTest {
 	@Test
 	public void testDivideWithoutOperator()
 	{
+		// TODO: test if divisor is 0, method returns error
+		// currently method returns type long, no fractional part
+		
 		assertTrue(1L == DivideWithoutOperator.divideWithoutOperator(1, 1));
-	}
+
+		// test expected result is zero
+		assertTrue(0L == DivideWithoutOperator.divideWithoutOperator(0, 1));
+		assertTrue(0L == DivideWithoutOperator.divideWithoutOperator(0, -1));
+		// 0/2 passes, because result 0 is type long.
+		assertTrue(0L == DivideWithoutOperator.divideWithoutOperator(0, 2));
+
+		// test truncate fractional portion of quotient
+		// TODO: Overload method to accept and return type double.
+		
+		// TODO: 1/2  should return 0.
+		// Currently it returns 1
+		//assertTrue(0L == DivideWithoutOperator.divideWithoutOperator(1, 2));
+		
+		assertTrue(2L == DivideWithoutOperator.divideWithoutOperator(2, 1));
+		assertTrue(3L == DivideWithoutOperator.divideWithoutOperator(3, 1));
+
+		
+		// test different combinations of sign
+		assertTrue(-3L == DivideWithoutOperator.divideWithoutOperator(-6, 2));
+		assertTrue(-3L == DivideWithoutOperator.divideWithoutOperator(6, -2));
+		assertTrue(3L == DivideWithoutOperator.divideWithoutOperator(-6, -2));
+}
 
 }
