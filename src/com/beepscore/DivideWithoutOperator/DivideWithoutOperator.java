26a777c00809c1a11c1ac28aa5c45df2f5eafe7d
beepscore <support@beepscore.com>
2010-12-11 16:54:42 -0800

    Implemented divideWithoutOperator.  TODO: Figure out how to return error if divisor = 0. Junit test passes.

--- src/com/beepscore/DivideWithoutOperator/DivideWithoutOperator.java
+++ src/com/beepscore/DivideWithoutOperator/DivideWithoutOperator.java
@@ -16,12 +16,36 @@ public class DivideWithoutOperator {
 		// TODO Auto-generated method stub
 
 	}
-	
+
 	// we can overload this method, changing the signature with different parameter lists
 	// http://download.oracle.com/javase/tutorial/java/javaOO/methods.html
 	public static long divideWithoutOperator(long dividend, long divisor)
-	{
-		long quotient = 0;
+	{	
+		// can't divide by zero
+		if (0 == divisor)
+		{
+			// throw error !!!!!!!!!!!!!!!!!!
+			return Long.MAX_VALUE;
+		}
+
+		long increment;
+		if ( -1 == (Math.signum(divisor) * Math.signum(dividend)) )
+		{
+			// quotient will be negative
+			increment = -1;
+		} else {
+			// quotient will be zero or positive
+			increment = 1;
+		}
+		long quotient;
+		for ( quotient = 0; (Math.abs(quotient) < Math.abs(dividend)); quotient += increment)
+		{
+			if ( Math.abs(quotient*divisor) >= Math.abs(dividend) )
+			{
+				return quotient;
+			}
+		}
+		// default fall through if loop condition doesn't work
 		return quotient;
 	}
 
